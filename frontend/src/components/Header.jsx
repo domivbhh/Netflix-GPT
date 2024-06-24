fb2d149cff9e6407e012b3374113dd523e976fc2
domivbhh <hariharanvbit123@gmail.com>
2024-06-24 12:18:09 +0530

    unsubscribe in useeffect

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,7 +22,7 @@ signOut(auth)
 
   }
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unSubscribe=onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -35,6 +35,9 @@ signOut(auth)
         navigate('/')
       }
     });
+    return ()=>{
+        unSubscribe()
+    }
   }, []);
 
 
